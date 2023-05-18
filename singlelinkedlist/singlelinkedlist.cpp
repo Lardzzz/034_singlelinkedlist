3daa8072af81b0ffdedc154cbd1ac3a7682bdb6e
Lardzzz <aku.ikan04@gmail.com>
2023-05-18 21:46:08 +0700

    menentukan kondisi yang berjalan dari fungsi addNode untuk memeriksa tidak ada duplikasi nim yang diinput

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -17,3 +17,17 @@ void addNode() {
 	cin >> nim;
 	cout << "Masukkan Nama: ";
 	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
